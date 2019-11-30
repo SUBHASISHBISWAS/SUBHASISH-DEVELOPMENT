bb8d3f74c6f900abab53da1299d7d44f49acfcbe
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2019-11-30 00:32:53 -0800

    Initilize

---
+++ SB_ASP.NET/1_ViewState_Demo/1_ViewState_Demo/About.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1_ViewState_Demo
+{
+
+
+    public partial class About : Page
+    {
+        int ClicksCount = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TextBox1.Text = "0";
+            }
+        }
+
+        protected void Buttonl_Click(object sender, EventArgs e)
+        {
+            ClicksCount=ClicksCount + 1;
+            TextBox1.Text = ClicksCount.ToString();
+        }
+    
+
+
+    }
+
+}
