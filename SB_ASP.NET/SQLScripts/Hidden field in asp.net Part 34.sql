c0a78aba693a77537e9be7f9079b9b19f3b7f246
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2019-12-01 09:22:52 -0800

    MVC Controlller

--- SB_ASP.NET/SQLScripts/Hidden field in asp.net Part 34.sql
+++ SB_ASP.NET/SQLScripts/Hidden field in asp.net Part 34.sql
@@ -9,3 +9,6 @@ Create Table tblEmployees
 Insert into tblEmployees values(201, 'Mark', 'Male', 'IT')
 Insert into tblEmployees values(202, 'Steve', 'Male', 'Payroll')
 Insert into tblEmployees values(203, 'John', 'Male', 'HR') 
+
+
+Select Id, Name, Gender, DeptName from tblEmployees
