183de5af6eaac9358d9c725bee11c19c31031679
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2019-12-03 06:20:07 -0800

    ControllerFactory

---
+++ SB_ASP.NET_MVC/SwigyFoodVendor/SwigyFoodVendor/ControllerFactory/SwigyControllerFactory.cs
@@ -0,0 +1,22 @@
+﻿using StructureMap;
+using SwigyFoodVendor.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SwigyFoodVendor.ControllerFactory
+{
+    public class SwigyControllerFactory : DefaultControllerFactory
+    {
+        protected override IController GetControllerInstance(RequestContext requestContext, 
+            Type controllerType)
+        {
+           
+            return Activator.CreateInstance(controllerType, new SqlServerLogger())
+               as IController;
+        }
+    }
+}
---
+++ SB_ASP.NET_MVC/SwigyFoodVendor/SwigyFoodVendor/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SwigyFoodVendor.Controllers
+{
+    public class HelloController : IController
+    {
+        ILogger _logger;
+        HelloController(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Execute(RequestContext requestContext)
+        {
+            requestContext.HttpContext.Response.Write("Hello");
+            _logger.Log("My Message");
+        }
+    }
+}
