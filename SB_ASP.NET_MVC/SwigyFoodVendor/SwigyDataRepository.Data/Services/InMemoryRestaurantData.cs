f9e1fcb630084bf350f8f75f8b79e7d5c2dac922
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2019-12-03 11:49:33 -0800

    View

--- SB_ASP.NET_MVC/SwigyFoodVendor/SwigyDataRepository.Data/Services/InMemoryRestaurantData.cs
+++ SB_ASP.NET_MVC/SwigyFoodVendor/SwigyDataRepository.Data/Services/InMemoryRestaurantData.cs
@@ -12,9 +12,9 @@ namespace SwigyDataRepository.Data.Services
         {
             restaurants = new List<Restaurant>()
             {
-                new Restaurant { Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian},
-                new Restaurant { Id = 2, Name = "Tersiguels", Cuisine = CuisineType.French},
-                new Restaurant { Id = 3, Name = "Mango Grove", Cuisine = CuisineType.Indian},
+                //new Restaurant { Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian},
+                //new Restaurant { Id = 2, Name = "Tersiguels", Cuisine = CuisineType.French},
+                //new Restaurant { Id = 3, Name = "Mango Grove", Cuisine = CuisineType.Indian},
             };
         }
 
