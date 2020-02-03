497f89c4cc6d2fcffa87070fa7755fe3733c11f1
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2020-02-02 22:08:08 -0800

    mODEL uPDATE

---
+++ SB_ASP.NET_MVC/SVLakeview/MyApartment/MyApartment/Pages/SVLakeview/EditExpense.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyApartment.Data.Services;
+using MyApartment.Model.Core.Models;
+
+namespace MyApartment
+{
+    public class EditExpenseModel : PageModel
+    {
+        private readonly IMyApartmentExpenseDataProvider _myExpenseDataProvider;
+
+        public EditExpenseModel(IMyApartmentExpenseDataProvider myExpenseDataProvider)
+        {
+            this._myExpenseDataProvider = myExpenseDataProvider;
+        }
+        public IMyApartmentExpense Expense { get; set; }
+        
+        public IActionResult OnGet(int expenseId)
+        {
+            Expense = _myExpenseDataProvider.GetExpenseDetailsById(expenseId);
+            if (Expense == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
