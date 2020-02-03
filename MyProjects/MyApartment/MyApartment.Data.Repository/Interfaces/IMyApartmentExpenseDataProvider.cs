f6a6f2775fab9b707a18c5b9f4c6be1f38e5d0bf
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2020-02-03 08:50:35 -0800

    Model and Service is Added

---
+++ MyProjects/MyApartment/MyApartment.Data.Repository/Interfaces/IMyApartmentExpenseDataProvider.cs
@@ -0,0 +1,21 @@
+﻿using MyApartment.Core.Model;
+using System;
+using System.Collections.Generic;
+
+namespace MyApartment.Data.Repository
+{
+    public interface IMyApartmentExpenseDataProvider
+    {
+        IEnumerable<IMyApartmentExpense> GetAllExpense();
+
+        IEnumerable<IMyApartmentExpense> GetExpenseByType(ExpenseType expenseType);
+
+        IMyApartmentExpense GetExpenseDetailsById(Guid id);
+
+        IMyApartmentExpense UpdateExpense(IMyApartmentExpense updatedExpense);
+
+        IMyApartmentExpense AddNewExpense(IMyApartmentExpense newExpense);
+
+        int Commit();
+    }
+}
---
+++ MyProjects/MyApartment/MyApartment.Data.Repository/MyApartmentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MyApartment.Core.Model;
+
+namespace MyApartment.Data.Repository
+{
+    public class MyApartmentDbContext :DbContext
+    {
+        public MyApartmentDbContext(DbContextOptions<MyApartmentDbContext> options):
+            base(options)
+        {
+                
+        }
+        public  DbSet<IMyApartmentExpense> Expenses { get; set; }
+    }
+}
