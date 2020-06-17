7148064aa341f029e47f10a7353053f40dfd318f
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2020-06-17 21:19:06 +0530

    ok

---
+++ Data Structure And Algorithm/SB_Tree/Binary_Serach_Tree/Node.js
@@ -0,0 +1,5 @@
+function Node(val) {
+  this.left = null;
+  this.right = null;
+  this.val = val;
+}
---
+++ Data Structure And Algorithm/SB_Tree/Binary_Serach_Tree/Tree.js
@@ -0,0 +1,9 @@
+function Tree() {
+  this.root = null;
+}
+
+Tree.prototype.addValue = function (val) {
+  if (root == null) {
+    this.root = val;
+  }
+};
