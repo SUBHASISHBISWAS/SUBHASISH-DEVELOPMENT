39fc9a318bc0b981137d44c5ef7f34d149acaa75
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2020-06-19 01:28:57 +0530

    NOC

---
+++ Real World Simulation/Procesing/1_Introduction/1_RandomWalker/RandomWalker/RandomWalker.pde
@@ -0,0 +1,21 @@
+class Walker 
+{
+  constructor() 
+  { 
+    this.x = width / 2;
+    this.y = height / 2; 
+  }
+}
+
+
+void setup()
+{
+  size(640, 400);
+}
+
+void draw()
+{
+  background(50);
+  ellipse(50,50,50,50);
+  
+}
---
+++ Real World Simulation/Procesing/1_Introduction/1_RandomWalker/RandomWalker/out/source/RandomWalker.java
@@ -0,0 +1,47 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class RandomWalker extends PApplet {
+
+class Walker 
+{
+  constructor() 
+  { 
+    this.x = width / 2;
+    this.y = height / 2; 
+  }
+}
+
+
+public void setup()
+{
+  
+}
+
+public void draw()
+{
+  background(50);
+  ellipse(50,50,50,50);
+  
+}
+  public void settings() {  size(640, 400); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "RandomWalker" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
