cea1c6c3add1778367c4b4074be0db77c26e933c
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2020-06-18 21:18:41 +0530

    Binary Search

--- Data Structure And Algorithm/SB_Tree/BST/sketch.js
+++ Data Structure And Algorithm/SB_Tree/BST/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  //createCanvas(windowWidth, windowHeight);
+  noCanvas();
+  CreateTree();
 }
 
-function draw() {
+function draw() {}
 
+var tree;
+function CreateTree() {
+  tree = new Tree();
+
+  for (i = 0; i < 10; i++) {
+    tree.addValue(floor(random(0, 100)));
+  }
+  /*
+  tree.addValue(10);
+  tree.addValue(5);
+  tree.addValue(1);
+
+  tree.addValue(6);
+  tree.addValue(3);
+  tree.addValue(30);
+*/
+  console.log(tree);
+
+  tree.traverse();
 }
