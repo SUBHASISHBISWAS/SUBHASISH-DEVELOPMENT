a8223cbe58c453bf8c65f2dd07350e02648472c6
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2020-10-20 15:40:48 +0530

    WEB API Database Connection

---
+++ WebAPI/SB_APIBasics/SB_APIBasics/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using APIDataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SB_APIBasics.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entity=new EmployeeDBEntities())
+            {
+                return entity.Employees.ToList();
+            }
+        }
+        public IEnumerable<Employee> Get(int id)
+        {
+            using (EmployeeDBEntities entity = new EmployeeDBEntities())
+            {
+                return entity.Employees.Where(e=>e.ID==id);
+            }
+        }
+    }
+}
