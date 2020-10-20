3b9ed1f0fd5f0a1de878e2be8d267531ff54c488
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2020-10-20 18:36:11 +0530

    Custom Formatters

--- WebAPI/SB_APIBasics/SB_APIBasics/App_Start/WebApiConfig.cs
+++ WebAPI/SB_APIBasics/SB_APIBasics/App_Start/WebApiConfig.cs
@@ -2,9 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 
 namespace SB_APIBasics
 {
+    public class CustomFormatter : JsonMediaTypeFormatter
+    {
+        public CustomFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, 
+            MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+
+    }
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -19,6 +39,18 @@ namespace SB_APIBasics
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Add(new CustomFormatter());
+            
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            /*
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = 
+                Formatting.Indented;
+            
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+            */
         }
     }
 }
