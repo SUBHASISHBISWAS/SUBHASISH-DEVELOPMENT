343f5d6d4fd1cd522883e79dd4af950419de5403
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2020-11-10 18:08:06 +0530

    SOLIDPrincipal

---
+++ Design_Pattern/SOLIDPrincipal/SOLIDPrincipal/OCP.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLIDPrincipal.OCP
+{
+
+    public class EmployeeDto
+    {
+        public string Name { get; set; }
+
+        public string SeniorityLavel { get; set; }
+
+        public double BillingRate { get; set; }
+
+        public int WorkingHours { get; set; }
+
+    }
+    public class EmpoyeeDataAccessLayer
+    {
+        public IEnumerable<EmployeeDto> GetEmployee()
+        {
+            // ORM to Get Data
+            return new List<EmployeeDto>
+            {
+                new EmployeeDto { Name="Subhasish",BillingRate=5, WorkingHours=8,SeniorityLavel="Developer"},
+                new EmployeeDto { Name="Adrita", BillingRate=10,WorkingHours=8,SeniorityLavel="Manager"}
+            };
+        }
+
+        public void Save(EmployeeDto employeeDto)
+        {
+            // Save the Employee
+        }
+
+
+    }
+
+    // Abstation Removed due to Seperation Consideration  
+    public interface IEmployee
+    {
+        string Name { get; set; }
+
+        double BillingRate { get; set; }
+
+        int WorkingHours { get; set; }
+
+        double EmpoyeeSalaryCalculator();
+        void HireNewEmployee();
+
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    // Logging (Cross cutting Concern) is Seperated from Domain Logic
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+
+    public abstract class Employee : IEmployee
+    {
+
+        protected string Name { get; set; }
+
+        protected double BillingRate { get; set; }
+
+        protected int WorkingHours { get; set; }
+
+        private EmpoyeeDataAccessLayer _empoyeeDataAccessLayer;
+
+        private ILogger _logger;
+
+        private double totalSalary = 0;
+
+        private IEnumerable<IEmployee> _employeeList;
+
+        public Employee(ILogger logger, IEnumerable<IEmployee> employees)
+        {
+
+            _logger = logger;
+            _employeeList = employees;
+            _empoyeeDataAccessLayer = new EmpoyeeDataAccessLayer();
+
+
+        }
+
+        public abstract double CalculateSalary();
+
+        public double EmpoyeeSalaryCalculator()
+        {
+
+            foreach (var employee in _employeeList)
+            {
+                totalSalary += CalculateSalary();
+                _logger.Log(string.Format("Employee Name :" + Name + ":" + "Salary" + totalSalary));
+                _empoyeeDataAccessLayer.Save(new EmployeeDto
+                {
+                    Name= employee.Name,
+                    BillingRate=employee.BillingRate,
+                    WorkingHours=employee.WorkingHours
+                });
+
+
+            }
+            return totalSalary;
+        }
+
+        // Not valid for all Employee 
+        public void HireNewEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public class Developer : Employee
+    {
+        
+        public double BillingRate { private set; get; }
+        double salary = 0;
+
+        public Developer(ILogger logger, IEnumerable<IEmployee> employees)
+            :base(logger,employees)
+        {
+
+        }
+        public override double CalculateSalary()
+        {
+            return salary += (WorkingHours* BillingRate) * 5;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public int WorkingHours { private set; get; }
+        public double BillingRate { private set; get; }
+        double salary = 0;
+
+        public Manager(ILogger logger, IEnumerable<IEmployee> employees)
+            : base(logger, employees)
+        {
+
+        }
+        public override double CalculateSalary()
+        {
+            return salary += (WorkingHours * BillingRate) * 10;
+        }
+    }
+}
---
+++ Design_Pattern/SOLIDPrincipal/SOLIDPrincipal/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLIDPrincipal
+{
+
+    public class EmpoyeeDto
+    {
+        public string Name { get; set; }
+
+        public  string SeniorityLavel { get; set; }
+
+        public double BillingRate { get; set; }
+
+        public int WorkingHours { get; set; }
+
+    }
+
+    public class EmpoyeeDataAccessLayer
+    {
+        public IEnumerable<EmpoyeeDto> GetEmployee()
+        {
+            // ORM to Get Data
+            return new List<EmpoyeeDto>
+            {
+                new EmpoyeeDto { Name="Subhasish",BillingRate=5, WorkingHours=8,SeniorityLavel="Developer"},
+                new EmpoyeeDto { Name="Adrita", BillingRate=10,WorkingHours=8,SeniorityLavel="Manager"}
+            };
+        }
+
+    }
+
+    // Come up with this abstraction @requirement analysis or at the time of development after
+    public interface IEmployee
+    {
+        double EmpoyeeSalaryCalculator();
+        void HireNewEmployee();
+        void Log(string message);
+    }
+
+    // Intial Abstraction and Encapsulation
+    public class Employee :IEmployee
+    {
+
+        // 1.Reason for Change
+        // This Persistance Layer will Chnage Over Time 
+        private EmpoyeeDataAccessLayer empoyeeDataAccessLayer;
+        
+        double totalSalary = 0;
+
+        public Employee()
+        {
+            // No validation 
+            empoyeeDataAccessLayer = new EmpoyeeDataAccessLayer();
+        }
+
+        public double EmpoyeeSalaryCalculator()
+        {
+            
+            var employees = empoyeeDataAccessLayer.GetEmployee();
+
+            foreach (var employee in employees)
+            {
+                // 2.Reason for Change
+                //This Business Logic Change Over Time 
+                switch (employee.SeniorityLavel)
+                {
+                    case "Developer":
+                        totalSalary += (employee.BillingRate * employee.WorkingHours)*5;
+                        break;
+                    case "Manager":
+                        totalSalary += (employee.BillingRate * employee.WorkingHours) *10;
+                        break;
+                    default:
+                        totalSalary += employee.BillingRate * employee.WorkingHours*1;
+                        break;
+                }
+
+                Log(string.Format("Employee Name :" + employee.Name + ":" + "Salary" + totalSalary));
+                
+                
+            }
+            return totalSalary;
+        }
+
+        // Not valid for all Employee 
+        public void HireNewEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        // 3.Reason for Change
+        // Logging (Cross cutting Concern and not Domain or Business Logic): 
+        public void Log( string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+   
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
