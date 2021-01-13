42474f97f3ca4b21e26330b09ebfeab06d2f2835
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-01-14 02:34:21 +0530

    iCloud support added

--- Apple_Development/ExpenseTracker/ExpenseTracker/Scene/AppDelegate.swift
+++ Apple_Development/ExpenseTracker/ExpenseTracker/Scene/AppDelegate.swift
@@ -35,29 +35,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "ExpenseTracker")
+        
+        let container = NSPersistentCloudKitContainer(name: "ExpenseTracker")
+        
+        let localStoreLocation = assetFilesDirectory(name: "ExpenseTracker/local.store", shouldCreate: true)!//
+        let localStoreDescription =
+                    NSPersistentStoreDescription(url: localStoreLocation)
+                localStoreDescription.configuration = "Local"
+        
+        let cloudStoreLocation  = assetFilesDirectory(name: "ExpenseTracker/icloud.store", shouldCreate: true)!
+               let cloudStoreDescription =
+                   NSPersistentStoreDescription(url: cloudStoreLocation)
+               cloudStoreDescription.configuration = "iCloud"
+        
+        
+        cloudStoreDescription.cloudKitContainerOptions =
+                    NSPersistentCloudKitContainerOptions(
+                        containerIdentifier: "iCloud.com.subhasishbiswas.expensetracker")
+        container.persistentStoreDescriptions = [
+                   cloudStoreDescription,
+                   localStoreDescription
+               ]
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        
         return container
     }()
 
@@ -74,6 +82,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
+        }
+    }
+    
+    func assetFilesDirectory(name: String, shouldCreate: Bool) -> URL? {
+        do {
+            let applicationSupportFolderURL = try FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let folder = applicationSupportFolderURL.appendingPathComponent(name)
+            if !FileManager.default.fileExists(atPath: folder.path) {
+                if shouldCreate {
+                    try FileManager.default.createDirectory(at: folder, withIntermediateDirectories: true, attributes: nil)
+                    print("ok")
+                } else {
+                    return nil
+                }
+            }
+
+            return folder
+        } catch {
+            print(error)
+            return nil
         }
     }
 
