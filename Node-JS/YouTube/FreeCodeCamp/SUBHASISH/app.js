6d3ba28150912470a7a7483631981e968518745b
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-05-29 16:46:05 +0530

    Express Introduction

--- Node-JS/YouTube/FreeCodeCamp/SUBHASISH/app.js
+++ Node-JS/YouTube/FreeCodeCamp/SUBHASISH/app.js
@@ -1,60 +1,29 @@
-const http = require('http');
-const {readFileSync} = require('fs')
+const express = require('express')
+const path = require('path');
 
-// Read Content of Index.html File 
-// All the static resource used in page served need to read and provide by Server
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyle = readFileSync('./navbar-app/styles.css')
-const homeLogo = readFileSync('./navbar-app/logo.svg')
-const browserApp = readFileSync('./navbar-app/browser-app.js')
 
-const server = http.createServer((req,res)=>{
+const app = express()
+//or 
+//const app = require('express')()
 
-    const url = req.url
+// Server static resouce requested by user while access ths page
+// i.e if index.html is served  and if index.html use some more resource 
+// it will server that resource
+// Setup static and middleware
+app.use(express.static('./public'))
 
-    if(url === "/"){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write(homePage)
-        res.end()
-    }
-    // Index.html uses static css file which need to provided by server on request
-    // Content type should be 'text/css'
-    else if(url === "/styles.css"){
-        res.writeHead(200,{'content-type':'text/css'})
-        res.write(homeStyle)
-        res.end()
-    }
-
-// Index.html uses static logo file which need to provided by server on request
-// Content type should be 'image/svg+xml'
-    else if(url === "/logo.svg"){
-        res.writeHead(200,{'content-type':'image/svg+xml'})
-        res.write(homeLogo)
-        res.end()
-    }
-
-    // Index.html uses javascript file which need to provided by server on request
-    // Content Type should be 'text/javascript'
-    else if(url === "/browser-app.js"){
-        res.writeHead(200,{'content-type':'text/javascript'})
-        res.write(browserApp)
-        res.end()
-    }
-    
-    else if(url === "/about"){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1> Hello About</h1>')
-        res.end()
-    }
-    else{
-        res.writeHead(404,{'content-type':'text/html'})
-        res.write('<h1> Page Not Found</h1>')
-        res.end()
-    }
-   
+// All Http Verb are Map to Method
+// This only Serve index.html but not the resource used inside index.html
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'navbar-app/index.html'))
 })
 
-server.listen(50000,()=>{
-    console.log("Server Started Lisning on POrt 50000");
+
+app.get('*',(req,res)=>{
+    res.status(404).send('<h1> Page Not Found </h1>')
 })
 
+
+app.listen(5000, ()=>{
+console.log('Server is listening on port 5000....');
+})
