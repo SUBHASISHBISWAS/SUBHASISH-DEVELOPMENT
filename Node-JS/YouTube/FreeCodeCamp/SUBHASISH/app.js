fe577e1fe5ebe2d40c2bce43802885bb660c54b5
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-05-29 22:17:54 +0530

    Query and URL paramater

--- Node-JS/YouTube/FreeCodeCamp/SUBHASISH/app.js
+++ Node-JS/YouTube/FreeCodeCamp/SUBHASISH/app.js
@@ -1,27 +1,75 @@
 const express = require('express')
 const path = require('path');
 
-
 const app = express()
-//or 
-//const app = require('express')()
 
-// Server static resouce requested by user while access ths page
-// i.e if index.html is served  and if index.html use some more resource 
-// it will server that resource
-// Setup static and middleware
-app.use(express.static('./public'))
+const {products} = require('./data')
 
-// All Http Verb are Map to Method
-// This only Serve index.html but not the resource used inside index.html
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'navbar-app/index.html'))
+res.send('<h1>Home Page </h1><a href="/api/products">Products</a>')
 })
 
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image}=product
+        return {id,name,image}
+    })
+    res.json(newProducts)
+    })
+// URL paramater   
+//  Get Product from Id passed as requestParamater
+    app.get('/api/products/:productId',(req,res)=>{
 
-app.get('*',(req,res)=>{
-    res.status(404).send('<h1> Page Not Found </h1>')
-})
+        //http://localhost:5000/api/products/1
+        // accessing url paramater
+        const {productId} = req.params
+        console.log(productId);
+    
+        // used find method to find product using id passed as paratmeter in url
+        const foundProduct = products.find (
+            (product)=>product.id === Number(productId))
+
+        if(!foundProduct){
+            return res.status(404).send('Product Not Exist')
+        }
+        return res.json(foundProduct)
+        })
+
+        // passing Multiple Params
+        //http://localhost:5000/api/products/1/reviews/10
+      app.get('/api/products/:productId/reviews/:reviewId',(req,res)=>{
+
+        const {productId, reviewId} = req.params
+        console.log(productId,reviewId);
+        res.send('Review')
+      })  
+
+
+      // passing Query Paramater 
+      //http://localhost:5000/api/v1/query?search=1&limit=2
+      app.get('/api/v1/query',(req,res)=>{
+
+        const {search,limit} = req.query
+        console.log(req.query);
+        let sortedProduct = [...products]
+
+        // Use Filter method to filter product passed as query string : query?search
+        if(search){
+            sortedProduct = sortedProduct.filter((product)=>{
+                return product.name.startsWith(search)
+            })
+        }
+
+        if(limit){
+            sortedProduct = sortedProduct.slice(0,Number(limit))
+        }
+
+        return res.status(200).json(sortedProduct)
+
+      })
+
+
+
 
 
 app.listen(5000, ()=>{
