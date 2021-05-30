63f5d8754ed40f9438ba0ca95792994860df72ff
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-05-31 00:51:47 +0530

    Task as State

--- React/YouTube/Traversy Media/React JS Crash Course 2021/SUBHASISH/task-tracker/src/components/Tasks.js
+++ React/YouTube/Traversy Media/React JS Crash Course 2021/SUBHASISH/task-tracker/src/components/Tasks.js
@@ -1,21 +1,22 @@
-import React from "react";
-
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "Feb 6th at 1:30pm",
-    reminder: true,
-  },
-];
+import React, { useState } from "react";
 
 export const Tasks = () => {
+
+    //Task as State 
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+  ]);
   return (
     <>
       {tasks.map((task) => (
