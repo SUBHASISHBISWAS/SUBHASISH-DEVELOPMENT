da362098b9e097eb4bd168cdacf5cfffd2d355a6
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-05-31 02:19:29 +0530

    Add  Font-Awesome Icon and Task Component

--- React/YouTube/Traversy Media/React JS Crash Course 2021/SUBHASISH/task-tracker/src/App.js
+++ React/YouTube/Traversy Media/React JS Crash Course 2021/SUBHASISH/task-tracker/src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
 import Header from "./components/Header";
 import { Tasks } from "./components/Tasks";
+import React, { useState } from "react";
 
+/**
+ * * This is my Min method
+ * ! hello
+ * ? will show
+ * todo: hello
+ * @returns
+ */
 function App() {
+  //! using state in functional component by using useState hook
+  //? Default state passed as Parameter to useState
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+  ]);
+
   return (
     <div className="container">
+      {/* //! This is Better Comment */}
       <Header title="Task Tracker" />
-      <Tasks/>
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- React/YouTube/Traversy Media/React JS Crash Course 2021/SUBHASISH/task-tracker/src/components/Tasks.js
+++ React/YouTube/Traversy Media/React JS Crash Course 2021/SUBHASISH/task-tracker/src/components/Tasks.js
@@ -1,26 +1,12 @@
 import React, { useState } from "react";
+import Task from "./Task";
 
-export const Tasks = () => {
-
-    //Task as State 
-  const [tasks, setTask] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-  ]);
+export const Tasks = ({ tasks }) => {
+  //setTask([...tasks, {}]);
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} />
       ))}
     </>
   );
