468e02f7735e2815146c59cbaa39d04be2d50189
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-05-30 19:08:04 +0530

    React Props

--- React/YouTube/CodeEvolution/react-basics/src/App.js
+++ React/YouTube/CodeEvolution/react-basics/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Greet from './Components/1-FunctionalComponent';
 import GreetClassComponent from './Components/2-GreetClassComponent';
 import JSXComponent from './Components/3-JSXComponent';
+import { ReactProps } from './4-FReactProps';
+import CReactProps from './5-CReactProps';
 
 function App() {
   return (
     <div className="App">
 
-     <JSXComponent/> 
+
+
+
+      {/* <CReactProps name ='Subhasish' title='Biswas'/>
+
+      <ReactProps name ='Subhasish' title='Biswas'>
+        <p> I am </p>
+      </ReactProps>
+      
+      <ReactProps name ='Subhasish' title='Biswas'/> */}
+      {/* <JSXComponent/>  */}
       {/* <GreetClassComponent/> */}
       {/* <Greet/> */}
       
