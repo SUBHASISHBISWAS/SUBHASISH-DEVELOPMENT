a8cc83343403f1df5a26c3da2894b45dbab15f2b
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-05-30 18:34:39 +0530

    React Basics

---
+++ React/YouTube/CodeEvolution/react-basics/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Components/1-FunctionalComponent';
+import GreetClassComponent from './Components/2-GreetClassComponent';
+import JSXComponent from './Components/3-JSXComponent';
+
+function App() {
+  return (
+    <div className="App">
+
+     <JSXComponent/> 
+      {/* <GreetClassComponent/> */}
+      {/* <Greet/> */}
+      
+    </div>
+  );
+}
+
+export default App;
