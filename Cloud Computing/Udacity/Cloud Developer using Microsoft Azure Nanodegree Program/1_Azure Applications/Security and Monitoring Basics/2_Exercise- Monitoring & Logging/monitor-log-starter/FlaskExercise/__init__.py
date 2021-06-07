54ce099aaffba64f605603e3cccaba6b86f0f1e9
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-06-07 17:27:49 +0530

    Commit

--- Cloud Computing/Udacity/Cloud Developer using Microsoft Azure Nanodegree Program/1_Azure Applications/Security and Monitoring Basics/2_Exercise- Monitoring & Logging/monitor-log-starter/FlaskExercise/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-from flask import Flask
-
-app = Flask(__name__)
-wsgi_app = app.wsgi_app
-# TODO: Set the app's logger level to "warning"
-#   and any other necessary changes
-
-import FlaskExercise.views
