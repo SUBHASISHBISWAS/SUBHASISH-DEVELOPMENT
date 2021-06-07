3d3306ea511dc16ef733d021fa307923c949e730
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-06-07 17:22:34 +0530

    commit

---
+++ Design_Pattern/Creationl_DesignPattern/BuilderPattern/PS_SimpleBuilderWithStatePattern/Main/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BuilderDemo.Builders.Person;
+using BuilderDemo.Interfaces;
+using BuilderDemo.Models;
+
+namespace BuilderDemo
+{
+    class Program
+    {
+
+        static void ConfigureUser()
+        {
+            PersonBuilder builder = new PersonBuilder();
+
+            builder.SetFirstName("Subhasish");
+            builder.SetLastName("Biswas");
+
+            IContactInfo contact = new EmailAddress("Subhasishbiswas15@gmail.com");
+            builder.Add(contact);
+
+            Person person = builder.Build();
+
+            Console.WriteLine(person);
+        }
+
+        
+        /* 
+        static void Main(string[] args)
+        {
+
+            ConfigureUser();
+
+            Console.WriteLine("Reached end of demonstration...");
+            Console.ReadLine();
+        }
+        */
+    }
+}
---
+++ Design_Pattern/Creationl_DesignPattern/BuilderPattern/PS_SimpleBuilderWithStatePattern/Models/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using BuilderDemo.Interfaces;
+
+namespace BuilderDemo.Models
+{
+    public class Person : IUser
+    {
+        public string Name { get; }
+        public string Surname { get; }
+        private IList<IContactInfo> Contacts { get; } = new List<IContactInfo>();
+        private IContactInfo PrimaryContact { get; set; }
+
+        public Person(string name, string surname)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException();
+            if (string.IsNullOrEmpty(surname))
+                throw new ArgumentException();
+
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public void SetIdentity(IUserIdentity identity) { }
+
+        public bool CanAcceptIdentity(IUserIdentity identity) =>
+            identity is IdentityCard;
+
+        public override string ToString() => $"{this.Name} {this.Surname}";
+
+        public void Add(IContactInfo contact)
+        {
+            if (contact==null)
+            {
+                throw new ArgumentException();
+            }
+            this.Contacts.Add(contact);
+        }
+
+        
+        public void SetPrimaryContact(IContactInfo contact)
+        {
+            if (contact == null)
+            {
+                throw new ArgumentException("");
+            }
+
+            if (!Contacts.Contains(contact))
+            {
+                throw new ArgumentException("");
+            }
+
+            PrimaryContact = contact;
+        }
+    }
+}
