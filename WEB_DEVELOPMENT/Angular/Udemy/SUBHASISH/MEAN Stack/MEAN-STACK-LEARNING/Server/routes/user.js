87492df4d644ececece265ee0781e02723f1c8d4
SUBHASISH <subhasishbiswas15@gmail.com>
2021-06-13 02:29:01 +0530

    jsonwebtoken jwt is added for Authentication

--- WEB_DEVELOPMENT/Angular/Udemy/SUBHASISH/MEAN Stack/MEAN-STACK-LEARNING/Server/routes/user.js
+++ WEB_DEVELOPMENT/Angular/Udemy/SUBHASISH/MEAN Stack/MEAN-STACK-LEARNING/Server/routes/user.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const user = require("../models/user");
+const { toEditorSettings } = require("typescript");
 
 const router = express.Router();
 
@@ -19,5 +22,37 @@ router.post("/signup", (req, res, next) => {
         res.status(500).json({ error: err });
       });
   });
+});
+
+router.post("/login", (req, res, next) => {
+  fetchedUser: User;
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Authentication Failed-User Not Available" });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({ message: "Authentication Failed" });
+      }
+
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "longer_string_need_to_passed",
+        { expiresIn: "1h" }
+      );
+      console.log("Hello Hi");
+      return res.status(200).json({ token: token });
+    })
+    .catch((err) => {
+      return res
+        .status(401)
+        .json({ message: "Authentication Failed-While Creating Token" });
+    });
 });
 module.exports = router;
--- WEB_DEVELOPMENT/Angular/Udemy/SUBHASISH/MEAN Stack/MEAN-STACK-LEARNING/src/app/auth/login/login.component.ts
+++ WEB_DEVELOPMENT/Angular/Udemy/SUBHASISH/MEAN Stack/MEAN-STACK-LEARNING/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,11 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   isLoading = false;
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
-  onLogin(form: NgForm) {}
+  onLogin(form: NgForm) {
+    this.authService.loginUser(form.value.email, form.value.password);
+  }
 }
