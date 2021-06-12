1aadbb791fdbe5482daa19cf3e0698a866d032c7
SUBHASISH <subhasishbiswas15@gmail.com>
2021-06-13 01:32:22 +0530

    User Added To Database

--- WEB_DEVELOPMENT/Angular/Udemy/SUBHASISH/MEAN Stack/MEAN-STACK-LEARNING/Server/routes/user.js
+++ WEB_DEVELOPMENT/Angular/Udemy/SUBHASISH/MEAN Stack/MEAN-STACK-LEARNING/Server/routes/user.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const user = require("../models/user");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = new user({
+    const user = new User({
       email: req.body.email,
       password: hash,
     });
     user
       .save()
       .then((result) => {
-        res.status(201).json({ message: "user Created", result: result });
+        res.status(201).json({ message: "User Created", result: result });
       })
-      .catch((result) => {
+      .catch((err) => {
         res.status(500).json({ error: err });
       });
   });
