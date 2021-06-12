3f4f1798753c1a9a16c26268639aa2c4f3bc46b8
SUBHASISH <subhasishbiswas15@gmail.com>
2021-06-13 01:09:44 +0530

    Password hash bcrypt package added

--- WEB_DEVELOPMENT/Angular/Udemy/SUBHASISH/MEAN Stack/MEAN-STACK-LEARNING/Server/routes/user.js
+++ WEB_DEVELOPMENT/Angular/Udemy/SUBHASISH/MEAN Stack/MEAN-STACK-LEARNING/Server/routes/user.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const user = require("../models/user");
+const bcrypt = require("bcrypt");
+
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-  const user = new user({
-    email: req.body.email,
-    password: req.body.password,
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new user({
+      email: req.body.email,
+      password: hash,
+    });
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({ message: "user Created", result: result });
+      })
+      .catch((result) => {
+        res.status(500).json({ error: err });
+      });
   });
 });
 module.exports = router;
